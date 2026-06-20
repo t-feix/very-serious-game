f9a5f81f4b160c177ebe3a3ad60d4ec4b5dfd252
timur-feix <vfdcz87d2d@privaterelay.appleid.com>
2026-06-20 11:48:48 +0200

    added player animation to script

--- player/player.gd
+++ player/player.gd
@@ -2,6 +2,7 @@ class_name Player
 extends CharacterBody2D
 
 @export var speed: float = 220.0
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(_delta: float) -> void:
 	#if RewindBuffer.is_rewinding():
@@ -12,6 +13,12 @@ func _physics_process(_delta: float) -> void:
 	var input_dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = input_dir * speed
 	
-	look_at(get_global_mouse_position())
+	if input_dir != Vector2.ZERO:
+		rotation = input_dir.angle() + PI/2
+		if not sprite.is_playing():
+			sprite.play("walk")
+	else:
+		sprite.stop()
+		sprite.frame = 0
 	
 	move_and_slide()
