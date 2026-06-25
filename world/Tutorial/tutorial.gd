63e260334def3a5e9eaed09919fd7a8ed21d6a3f
timur-feix <vfdcz87d2d@privaterelay.appleid.com>
2026-06-25 22:29:16 +0200

    fix hopefully

--- world/Tutorial/tutorial.gd
+++ world/Tutorial/tutorial.gd
@@ -5,7 +5,8 @@ extends Node2D
 
 func _ready() -> void:
 	GameState.last_level_path = scene_file_path
-	
+	EventBus.button_pressed.connect(_on_button_pressed)
+		
 	var player := get_node_or_null("Player") as Player
 	if player == null:
 		return
@@ -16,3 +17,14 @@ func _ready() -> void:
 		player.global_position = spawn_from_lower
 	
 	GameState.spawn_from = ""
+
+func _on_button_pressed(button_id: int) -> void:
+	match button_id:
+		0:
+			EventBus.door_lock_changed.emit(2, false)
+			EventBus.door_lock_changed.emit(3, false)
+		1:
+			EventBus.door_lock_changed.emit(6, false)
+			EventBus.door_lock_changed.emit(7, false)
+		_:
+			push_warning("Unhandled button press: %d" % button_id)
